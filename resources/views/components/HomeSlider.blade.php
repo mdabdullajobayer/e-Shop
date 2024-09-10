08a6c1e365a5420e2463f61b0ac059b244011f3c
mdabdullajobayer <mdabdullajovayer@gmail.com>
2024-09-11 00:05:12 +0600

    Home Slider Load by Axios

--- resources/views/components/HomeSlider.blade.php
+++ resources/views/components/HomeSlider.blade.php
@@ -1,27 +1,8 @@
 <!-- START SECTION BANNER -->
 <div class="banner_section slide_medium shop_banner_slider staggered-animation-wrap">
     <div id="carouselExampleControls" class="carousel slide carousel-fade light_arrow" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item background_bg active" data-img-src="assets/images/banner16.jpg">
-                <div class="banner_slide_content banner_content_inner">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-7 col-10">
-                                <div class="banner_content overflow-hidden">
-                                    <h2 class="staggered-animation" data-animation="slideInLeft"
-                                        data-animation-delay="0.5s">LED 75 INCH F58</h2>
-                                    <h5 class="mb-3 mb-sm-4 staggered-animation font-weight-light"
-                                        data-animation="slideInLeft" data-animation-delay="1s">Get up to <span
-                                            class="text_default">50%</span> off Today Only!</h5>
-                                    <a class="btn btn-fill-out staggered-animation text-uppercase"
-                                        href="shop-left-sidebar.html" data-animation="slideInLeft"
-                                        data-animation-delay="1.5s">Shop Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="carousel-inner" id="Herobanner">
+
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev"><i
                 class="ion-chevron-left"></i></a>
@@ -30,3 +11,40 @@
     </div>
 </div>
 <!-- END SECTION BANNER -->
+
+<script>
+    LoadHeroBanner();
+    async function LoadHeroBanner() {
+
+        let res = await axios.get('/list-product-slider');
+        $('#Herobanner').empty();
+        res.data['data'].forEach((item, i) => {
+            let activeclass = '';
+            if (i == 0) {
+                activeclass = 'active';
+            }
+            let bannerItem = `
+         <div class="carousel-item background_bg ${activeclass}" style="background-image: url('${item['image']}')">
+                <div class="banner_slide_content banner_content_inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7 col-10">
+                                <div class="banner_content overflow-hidden">
+                                    <h2 class="staggered-animation" data-animation="slideInLeft"
+                                        data-animation-delay="0.5s">${item['title']}</h2>
+                                    <h5 class="mb-3 mb-sm-4 staggered-animation font-weight-light"
+                                        data-animation="slideInLeft" data-animation-delay="1s">${item['short_des']}</h5>
+                                    <a class="btn btn-fill-out staggered-animation text-uppercase"
+                                        href="#" data-animation="slideInLeft"
+                                        data-animation-delay="1.5s">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+            $('#Herobanner').append(bannerItem);
+        });
+    }
+</script>
